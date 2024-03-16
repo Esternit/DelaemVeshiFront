1436a0296600c1b1bee774243ed928b8b5d667da
Esternit <92298236+Esternit@users.noreply.github.com>
2024-03-16 10:33:01 +0300

    cart updt

--- scripts/loadinfo.js
+++ scripts/loadinfo.js
@@ -152,6 +152,19 @@ Telegram.WebApp.onEvent("mainButtonClicked", function () {
         body: item
     })
 
+    const Id = new URLSearchParams(window.location.search).get('id');
+    const paging = new URLSearchParams(window.location.search).get('page');
+    const sh = new URLSearchParams(window.location.search).get('search');
+    if (sh != null) {
+        window.location.href = 'search.html?page=' + paging + "&spuds=" + Id + "&search=" + sh;
+    }
+    else {
+        doning = 1;
+        window.location.href = 'index.html?page=' + paging + "&spuds=" + Id;
+    }
+
+    BackButton.hide();
+    tg.MainButton.hide();
 });
 
 function test() {

6c89bc18f3310c64ddd76cb07ace0d31a55caf7a
Ignatij3 <ignatijs.krasovskis@gmail.com>
2024-04-15 15:39:13 +0200

    Added only number support for entry field

--- scripts/cart-bullets.js
+++ scripts/cart-bullets.js
@@ -21,3 +21,34 @@ function changeStatus(cart_button_id) {
     toggleCheckFor(cart_button_id.replace(/-button+$/, ''));
     console.log(window.getComputedStyle(list_bullet, '::marker').getPropertyValue("content"));
 }
+
+function setInputFilter(textbox, inputFilter) {
+    ["input", "keydown", "keyup", "mousedown", "mouseup", "select", "contextmenu", "drop", "focusout"].forEach(function (event) {
+        textbox.addEventListener(event, function (e) {
+            if (inputFilter(this.value)) {
+                // Accepted value
+                if (["keydown", "mousedown", "focusout"].indexOf(e.type) >= 0) {
+                    this.classList.remove("input-error");
+                }
+                this.oldValue = this.value;
+                this.oldSelectionStart = this.selectionStart;
+                this.oldSelectionEnd = this.selectionEnd;
+
+            } else if (this.hasOwnProperty("oldValue")) {
+                // Rejected value - restore the previous one
+                this.classList.add("input-error");
+                this.value = this.oldValue;
+                this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+            } else {
+                // Rejected value - nothing to restore
+                this.value = "";
+            }
+        });
+    });
+}
+
+
+// Install input filters.
+setInputFilter(document.getElementById("phone-number"), function (value) {
+    return /^\+?\d*(\d[ ]?\d*){0,6}$/.test(value);
+});

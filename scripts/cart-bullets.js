f0290cacdb4db076580b7a4db440b0e40b3903ab
Ignatij3 <ignatijs.krasovskis@gmail.com>
2024-04-16 10:15:05 +0200

    Changed all svgs, fixed check scripts

--- scripts/cart-bullets.js
+++ scripts/cart-bullets.js
@@ -1,45 +1,15 @@
-var styleSameAddress = document.head.appendChild(document.createElement("style"));
-var stylePickupMoscow = document.head.appendChild(document.createElement("style"));
-var styleSameName = document.head.appendChild(document.createElement("style"));
-var styleSameNumber = document.head.appendChild(document.createElement("style"));
-
-function toggleCheckFor(list_id) {
-    const currentStyle = window.getComputedStyle(document.getElementById(list_id), '::marker').getPropertyValue("content");
-
-    var styleElem;
-    if (currentStyle == "url(\"https://raw.githubusercontent.com/Esternit/DelaemVeshiFront/main/assets/list_bullet.svg\")") {
-        styleElem = "#" + list_id + "::marker {content: url(\"https://raw.githubusercontent.com/Esternit/DelaemVeshiFront/main/assets/list_bullet_checked.svg\");}";
-    } else {
-        styleElem = "#" + list_id + "::marker {content: url(\"https://raw.githubusercontent.com/Esternit/DelaemVeshiFront/main/assets/list_bullet.svg\");}";
-    }
-
-    switch (list_id) {
-        case 'same-address':
-            styleSameAddress.innerHTML = styleElem;
-            break;
-
-        case 'pickup-moscow':
-            stylePickupMoscow.innerHTML = styleElem;
-            break;
-
-        case 'same-name':
-            styleSameName.innerHTML = styleElem;
-            break;
-
-        case 'same-number':
-            styleSameNumber.innerHTML = styleElem;
-            break;
-    }
-}
-
 /* 
     passed id is of format: ${reference-name}-button
     list element id is of format: ${reference-name}
 */
 function changeStatus(cart_button_id) {
-    const list_bullet = document.getElementById(cart_button_id.replace(/-button+$/, ''));
-
-    toggleCheckFor(cart_button_id.replace(/-button+$/, ''));
+    var bullet = document.getElementById(cart_button_id.replace(/-button+$/, ''));
+    console.log(bullet.src);
+    if (bullet.src == "https://raw.githubusercontent.com/Esternit/DelaemVeshiFront/main/assets/list_bullet.svg") {
+        bullet.src = "https://raw.githubusercontent.com/Esternit/DelaemVeshiFront/main/assets/list_bullet_checked.svg";
+    } else {
+        bullet.src = "https://raw.githubusercontent.com/Esternit/DelaemVeshiFront/main/assets/list_bullet.svg";
+    }
 }
 
 function setInputFilter(textbox, inputFilter) {

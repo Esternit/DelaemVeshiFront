eae00ac9db32045a576f1847ee84a1f547638612
Ignatij3 <ignatijs.krasovskis@gmail.com>
2024-04-26 09:30:24 +0200

    Bullets should appear for order page

--- scripts/cart-bullets.js
+++ scripts/cart-bullets.js
@@ -3,6 +3,7 @@
     list element id is of format: ${reference-name}
 */
 function changeStatus(cart_button_id) {
+    console.log("cart_button_id", cart_button_id);
     var bullet = document.getElementById(cart_button_id.replace(/-button+$/, ''));
     console.log(bullet.src);
     if (bullet.src == "https://raw.githubusercontent.com/Esternit/DelaemVeshiFront/main/assets/list_bullet.png") {

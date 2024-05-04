c7c75564aab8b015c3c8a65318729bc85ab4882f
Esternit <92298236+Esternit@users.noreply.github.com>
2024-05-04 16:58:08 +0300

    added header tests

--- scripts/header-info.js
+++ scripts/header-info.js
@@ -41,3 +41,32 @@ function loadHeaderHTML(data){
 function loadOneHeader(){
     document.getElementById("cart-item-amount-badge").innerText = `${items}`;
 }
+
+function checkHeader(){
+    fetch('https://crmback-production.up.railway.app/getUserBaseInfo', {
+        headers: {
+            'Content-type': 'application/json'
+        },
+        method: 'POST',
+        body: JSON.stringify({ user_id: window.Telegram.WebApp.initDataUnsafe.user.id })
+    })
+        .then(response => response.json())
+        .then(data => {
+            checkDATA(data['data']);
+        });
+}
+
+
+function checkDATA(data){
+    var bonus = localStorage.getItem('bonus');
+    var items = localStorage.getItem('items');
+
+    if(data["bonus"][0]["bonus_adds_owner"] != bonus || data["base"][0]["COUNT(*)"] != items){
+        localStorage.setItem('bonus', data["bonus"][0]["bonus_adds_owner"]);
+        localStorage.setItem('items', data["base"][0]["COUNT(*)"]);
+        console.log("test failed", data["base"][0]["COUNT(*)"], items)
+        loadHeaderHTML(data);
+    }
+}
+
+checkHeader();

1ff5826491827b905e056d17dd17c5069caa23e3
Ignatij3 <ignatijs.krasovskis@gmail.com>
2024-05-04 13:52:40 +0200

    revert test

--- scripts/item-card-functionality.js
+++ scripts/item-card-functionality.js
@@ -1,7 +1,23 @@
+function addItemToCart(id) {
+    var popup = document.getElementById("cart-popup");
+    popup.style.transform = "translateY(0%)";
 
+    var cart = document.getElementById("cart-container");
+    cart.style.opacity = "0";
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+
+    setTimeout(function () {
+        cart.style.visibility = "hidden";
+    }, 500);
+
+    setTimeout(function () {
+        popup.style.transform = "translateY(-100%)";
+        cart.style.visibility = "visible";
+        cart.style.opacity = "1";
+    }, 15000);
+}
 
 jQuery(document).ready(function () {
-    alert("jquery loaded");
     $("#sizing").delegate(".size", "click", function (event) {
         console.log("before toggle");
         jQuery('.size').removeClass('active');
@@ -16,3 +32,10 @@ jQuery(document).ready(function () {
 });
 
 console.log("called");
+fetch('https://crmback-production.up.railway.app/addToCart', {  //https://crmback-production.up.railway.app
+    headers: {
+        'Content-type': 'application/json'
+    },
+    method: 'POST',
+    body: item
+});

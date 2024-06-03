21982d4590400032e7e273f04f0d04cbb2dd56b8
Esternit <92298236+Esternit@users.noreply.github.com>
2024-06-03 15:48:12 +0300

    + bonus page backend

---
+++ scripts/bonus_info.js
@@ -0,0 +1,40 @@
+function loadBonusInfo() {
+    fetch('https://rmstoreapi-production.up.railway.app/getBonusInfo', {
+        headers: {
+            'Content-type': 'application/json'
+        },
+        method: 'POST',
+        body: JSON.stringify({ user_id: window.Telegram.WebApp.initDataUnsafe.user.id })  //window.Telegram.WebApp.initDataUnsafe.user.id
+    })
+        .then(response => response.json())
+        .then(data => loadHTMLTable(data));
+}
+
+function loadHTMLTable(data) {
+    document.getElementById("bonuses-body").innerHTML = `
+    <span class="page-title">Бонусы</span>
+	<div style="margin-top: 2rem;">
+		<div class="bonus-badge">
+			<span class="bonus-title">Твой баланс бонусов: </span>
+			<span class="bonus-amount">${data["bonuses"]}</span>
+			<div class="bonuses-currency">₽</div>
+		</div>
+		<div class="bonus-badge">
+			<span class="bonus-title">Сколько друзей ты привел: </span>
+			<span class="bonus-amount">${data["entered"]}</span>
+		</div>
+		<div class="bonus-badge">
+			<span class="bonus-title">Сколько товаров купили твои пришедшие друзья: </span>
+			<span class="bonus-amount">${data["amountofusers"]}</span>
+		</div>
+		<div class="bonus-badge">
+			<span class="bonus-title">Твои бонусы за месяц: </span>
+			<span class="bonus-amount">${data["selfBonusMonth"]}</span>
+			<div class="bonuses-currency">₽</div>
+		</div>
+	</div>
+	<script src="./scripts/bonus_info.js"></script>
+    `;
+}
+
+loadBonusInfo();
